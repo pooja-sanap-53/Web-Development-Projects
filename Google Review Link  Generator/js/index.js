70c7a4d33978f8b4f28de7e1bf4c19e39a52c7b1
Pooja Sanap <sanappooja777@gmail.com>
2022-03-27 13:54:43 +0530

    delted

--- Google Review Link  Generator/js/index.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("main-nav");
-    if (x.className === "main-nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "main-nav";
-    }
-  }
